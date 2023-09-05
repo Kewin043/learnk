ff68939789e3ec1921c1bf01c2294386b801d87a
Kewin043 <143746027+Kewin043@users.noreply.github.com>
2023-09-05 18:07:07 +0530

    Add files via upload

---
+++ as_08.py
@@ -0,0 +1,9 @@
+list1=[1,2,3,4,5]
+list2 = []
+for num in list1:
+    if (num % 2)== 0:
+        print('even')
+    else:
+        print('odd')
+list2
+
---
+++ assignment_8.py
@@ -0,0 +1,13 @@
+def list_values(list1):
+
+     for num in list1:
+         if (num % 2) == 0:
+             print('even')
+         else :
+             print('odd')
+
+          
+list1=[1,2,3,4,5]
+list2 = [list_values(list1)]
+
+    
