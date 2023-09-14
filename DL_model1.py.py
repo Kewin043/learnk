0e3da17c633103687387cb50c4fa0fe0a7d2cb20
Kewin043 <143746027+Kewin043@users.noreply.github.com>
2023-09-14 16:19:04 +0530

    Update DL_model1.py.py

--- DL_model1.py.py
+++ DL_model1.py.py
@@ -1,3 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axed3D
+from mpl_toolkits.mplot3d import Axes3D
+
+observation = 1000
+xs = np.random.uniform(-10,10,(observation,1))
+zs = np.random.uniform(-10,10,(observation,1))
+input = np.column_stack((xs,zs))
+print(input.shape)
+
+noise = np.random.uniform(-1,1,(observation,1))
+targets = 13*xs+7*zs - 12 + noise
+print(targets.shape)
+
+tragets = targets.reshape(observation,)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(xs,zs,targets)
+ax.set_xlabel('xs')
+ax.set_ylabel('zs')
+ax.set_zlabel('Targets')
+ax.view_init(azim = 100)
+plt.show()
+tragets = tragets.reshape(observation ,1)
+
+init_range = 0.1
+weights = np.random.uniform(-init_range,init_range,size=(2,1))
+biases = np.random.uniform(-init_range,init_range,size=1)
+print(weights)
+print(biases)
+
+learning_rate= 0.02
+
+for i in range(100):
+    outputs = np.dot(input,weights) + biases
+    deltas = outputs - targets
+
+    loss = np.sum(deltas**2)/2/observation
+    print(loss)
+    deltas_scaled = deltas/observation
+    weights = weights - learning_rate*np.dot(input.T,deltas_scaled)
+    biases = biases - learning_rate*np.sum(deltas_scaled)
+    print(weights,biases)
+    plt.plot(outputs,tragets)
+    plt.xlabel('outputs')
+    plt.ylabel('targets')
+    plt.show
